6d60a2eca68fbfd8c5e4f8670fb9c1082303aa9f
olterman <patrik@olterman.se>
2026-01-09 13:16:15 +0100

    Fixed inline CSS

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ It is a work in progress and my first attempt att creating a logseq plugin ...
 I give thanks to @bergbrains for his [ChordproJSParser](https://github.com/bergbrains/ChordproJSParser)
 
 after adding the plugin to logseq any page with a block that contains: 
-```[[logseq]]
+```[[chordpro]]
 {title: some cool song}
 {artist: some cool artist}
 
@@ -14,7 +14,25 @@ and som[C]e chor[Am]dpro content
 ``` 
 will render an html block with text/chords 
 
-TODO
-- add css that actually shows in logseq (today I add the CSS by hand in custom CSS)
-- make the html replace the chordpro on the page instead of in a sub block 
+## Prerequisites
+You need to have [node](https://nodejs.org/) and [npm or yarn](https://yarnpkg.com/getting-started/install) installed on your system.
+
+## Manual Installation 
+- clone this repo and build the plugin 
+>```
+git clone https://github.com/olterman/logseq-chordpro-render.git
+cd logseq-chordpro-render
+npm install 
+npm run build 
+```
+- open Logseq Desktop client and turn on Developer mode in user settings panel
+- open the toolbar dot menus and navigate to plugins page
+- navigate to the plugins dashboard: tp
+- click Load unpacked plugin button, then select the logseq-chordpro-render directory to load it 
+
+## TODO
+- add Style settings 
+- move render to the top of page without destroying outline structure
+- make the html replace the chordpro on the page instead of in a sibling block 
+- change polling so it doesnt trigger when editing a block (currently polling is disabled)
 
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,12 @@ import ChordproJS from 'chordprojs';
 let pollTimer: number | null = null;
 const seenHashes = new Map<string, string>();
 
+// CSS settings 
+const chordFormating = ""
+const lyricsColor = ""
+
+
+
 function log(...args: any[]) {
   console.log("🎵 [ChordPro]", ...args);
 }
@@ -28,23 +34,99 @@ function sanitizeChordPro(input: string): string {
     .trim();
 }
 
+function makeShiny(input: string): string {
+  /* CHORDPRO CSS 
+  Main CSS Classes
+    .chord-line - Applied to chord lines (pre element)
+    .lyric-line - Applied to lyric lines (pre element)
+*/
+  const preStyle = "background:none; padding: 0em 0em 0em 0em; margin: 0em 0 0em 0em;"
+  const chordStyle = "color: #c678dd; font-wight: bold;"
+  const lyricStyle = "color: #abb2bf;"
+    //.lyric-line-only - Applied to lyric lines when chords are hidden
+    //.comment - Applied to comment lines
+    //.comment-italic - Applied to italic comment lines
+    //.comment-box - Applied to boxed comment lines
+    //.highlight - Applied to highlighted text
+    //.section - Applied to all section containers
+    //.section-label - Applied to section labels
+    //.chorus - Applied to chorus containers
+    //.verse - Applied to verse containers
+    //.bridge - Applied to bridge sections
+    //.tab - Applied to tab sections
+    //.grid - Applied to grid sections
+    //.abc - Applied to ABC notation sections
+    //.ly - Applied to LilyPond notation sections
+    //.svg - Applied to SVG content sections
+    //.textblock - Applied to text block sections
+    //.chorus-ref - Applied to chorus references
+    //.chord-diagram - Applied to chord diagrams
+    //.chord-name - Applied to chord names in diagrams
+    //.chord-definition - Applied to chord definitions in diagrams
+    //.page-break - Applied to page breaks
+    //.physical-page-break - Applied to physical page breaks
+    //.column-break - Applied to column breaks
+    //.image - Applied to image containers
+    const emptyLineStyle ="background:none; padding: 0em 0em 0em 0em; margin: 0em 0 0em 0em;";
+    //.artist - Applied to artist information
+    //.key - Applied to key information
+  return input
+    .replaceAll("<pre class=\"chord-line\">", "<pre class=\"chord-line\" style=\"" + preStyle + chordStyle + "\">")
+    .replaceAll("<pre class=\"lyric-line\">", "<pre class=\"lyric-line\" style=\"" + preStyle + lyricStyle + "\">")
+    //.lyric-line-only - Applied to lyric lines when chords are hidden
+    //.comment - Applied to comment lines
+    //.comment-italic - Applied to italic comment lines
+    //.comment-box - Applied to boxed comment lines
+    //.highlight - Applied to highlighted text
+    //.section - Applied to all section containers
+    //.section-label - Applied to section labels
+    //.chorus - Applied to chorus containers
+    //.verse - Applied to verse containers
+    //.bridge - Applied to bridge sections
+    //.tab - Applied to tab sections
+    //.grid - Applied to grid sections
+    //.abc - Applied to ABC notation sections
+    //.ly - Applied to LilyPond notation sections
+    //.svg - Applied to SVG content sections
+    //.textblock - Applied to text block sections
+    //.chorus-ref - Applied to chorus references
+    //.chord-diagram - Applied to chord diagrams
+    //.chord-name - Applied to chord names in diagrams
+    //.chord-definition - Applied to chord definitions in diagrams
+    //.page-break - Applied to page breaks
+    //.physical-page-break - Applied to physical page breaks
+    //.column-break - Applied to column breaks
+    //.image - Applied to image containers
+    .replaceAll("<div class=\"empty-line\">", "<div class=\"empty-line\" style=\"" + emptyLineStyle + "\">")
+    //.artist - Applied to artist information
+    //.key - Applied to key information
+    //.replaceAll("</pre>", "</pre>\n")
+    
+}
+
 async function detectAndRender(reason: string) {
   log("Detect triggered:", reason);
+  //const editing = logseq.Editor.checkEditing();
+  //const isEditing = await logseq.App.getStateFromStore('editor/in-composition?');
+  //log("isEditing:", isEditing )
+  //if ( editing ){
+    //log("User is editing, wait");
+    //return;
+ // } else {
+    const page = await logseq.Editor.getCurrentPage();
+    if (!page || typeof page.uuid !== "string") return;
 
-  const page = await logseq.Editor.getCurrentPage();
-  if (!page || typeof page.uuid !== "string") return;
-
-  const blocks = await logseq.Editor.getPageBlocksTree(page.uuid);
-  if (!blocks) return;
-
-  // recursively find first block containing [[chordpro]]
+    const blocks = await logseq.Editor.getPageBlocksTree(page.uuid);
+    if (!blocks) return;
+  //}
+  // recursively find first block containing "[chordpro]]" or "{title:"
+  // To allocate chordpro conten
   function find(nodes: any[]): any | null {
     for (const b of nodes) {
       if (
         typeof b.content === "string" &&
-        
-        b.content.toLowerCase().includes("[[chordpro]]")
-        //b.content.toLowerCase().includes("{title:[^}]*\}")
+        b.content.toLowerCase().includes("[[chordpro]]") ||
+        b.content.toLowerCase().includes("{title:")
       ) {
         return b;
       }
@@ -69,17 +151,16 @@ async function detectAndRender(reason: string) {
   seenHashes.set(block.uuid, contentHash);
 
   log("FOUND chordpro block:", block.uuid);
-  log("Sanitized source:\n", source);
 
   let html; 
   try {
     // add a newline at the end to avoid JS-ChordPro parse errors
     const doc = sanitizeChordPro(source);
+    //g("Sanitized source:\n", doc);
     //const style = "<head><style>.chord-line { color: #0066cc; font-weight: bold; } .title {color: #e06c75;  /* Pink for title */}</style></head>";
-    html = chordpro.renderToHTML(doc);
+    html = makeShiny(chordpro.renderToHTML(doc));
     // html = style + chordpro.renderToHTML(doc);
     // After rendering
-    document.querySelectorAll('.chord-line').forEach(el => { el.style.color = '#0066cc';});
 
   } catch (err) {
     console.error("🎵 [ChordPro] Render error", err, { source });
@@ -88,16 +169,24 @@ async function detectAndRender(reason: string) {
 
 	// remove previously rendered HTML children
 	try {
-  		const children = await logseq.Editor.getBlockChildren(block.uuid);
+      const children = await logseq.Editor.getCurrentPageBlocksTree();
   		for (const child of children ?? []) {
-    		if (typeof child.content === "string" && child.content.startsWith("<h1>")) {
-            log("FOUND child -> killing it", child.uuid);
-      			await logseq.Editor.removeBlock(child.uuid);
-    		}
+        //log("FOUND child", child.content);
+    		//if (typeof child.content === "string" && child.content.includes("[[chordpro]]")) {
+          //logseq.Editor.setBlockCollapsed(child.uuid, true);
+          const sibling = await logseq.Editor.getNextSiblingBlock(block.uuid); 
+          //log(" '----> sibling content", sibling?.content)
+          if ( typeof sibling?.content === "string" && sibling.content.includes("<h1>")) { 
+          log("FOUND sibling with HTML-> killing it", sibling.uuid);
+          //log("sibling content", sibling.content)
+          await logseq.Editor.removeBlock(sibling.uuid);
+          }
+      
   		}
 	} catch (err) {
   			console.warn("🎵 [ChordPro] Could not fetch children, skipping removal", err);
 	}
+
 
 
   // insert rendered HTML
@@ -105,10 +194,21 @@ async function detectAndRender(reason: string) {
     block.uuid,
     //`\`\`\`html\n${html}\n\`\`\``,
     `${html}\n`,
-    //{ sibling: false }
+    { sibling: true }   
   );
 
+ /* If we need to reinsert the chordpro content 
+
+    await logseq.Editor.insertBlock(
+    block.uuid,
+    //`\`\`\`html\n${html}\n\`\`\``,
+    `${source}\n`,
+    { sibling: true }
+     
+  );*/
+
   log("Rendered successfully");
+  
 }
 
 function startPolling() {
@@ -128,7 +228,8 @@ async function main() {
     detectAndRender("route-change");
   });
 
-  startPolling();
+  //logseq.App.onBlockRendererSlotted()
+  //startPolling();
 }
 
 logseq.ready(main);
